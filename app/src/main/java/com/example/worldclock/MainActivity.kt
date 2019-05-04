a471dd3b36d4992883668eac1853f899ffeb5441
Yuki Sumida <y.sumida.0201@gmail.com>
2019-05-04 17:52:46 +0900

    ユーザーのタイムゾーンを表示する

--- app/src/main/java/com/example/worldclock/MainActivity.kt
+++ app/src/main/java/com/example/worldclock/MainActivity.kt
@@ -2,11 +2,17 @@ package com.example.worldclock
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val defaultTimezone = TimeZone.getDefault()
+
+        timezone.text = defaultTimezone.displayName
     }
 }
