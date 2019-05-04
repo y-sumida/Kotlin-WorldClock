c946bfff50e713085d3409c5e81d26741dd1412c
Yuki Sumida <y.sumida.0201@gmail.com>
2019-05-05 00:50:39 +0900

    タイムゾーン選択画面を実装する

--- app/src/main/java/com/example/worldclock/TimezoneSelectActivity.kt
+++ app/src/main/java/com/example/worldclock/TimezoneSelectActivity.kt
@@ -1,12 +1,31 @@
 package com.example.worldclock
 
+import android.app.Activity
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_timezone_select.*
 
 class TimezoneSelectActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_timezone_select)
+
+        setResult(Activity.RESULT_CANCELED)
+
+        val adapter = TimezoneAdapter(this)
+        clockList.adapter = adapter
+
+        clockList.setOnItemClickListener { _, _, position, _ ->
+            val timezone = adapter.getItem(position)
+
+            val result = Intent()
+            result.putExtra("timezone", timezone)
+
+            setResult(Activity.RESULT_OK, result)
+
+            finish()
+        }
     }
 }
